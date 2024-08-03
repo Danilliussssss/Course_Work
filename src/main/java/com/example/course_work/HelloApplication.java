ea5400b9e31d00ad794356475fad82f92e9fef2e
Danilliussssss <72625902+Danilliussssss@users.noreply.github.com>
2024-08-03 17:33:51 +0700

    Add files and repository

--- src/main/java/com/example/course_work/HelloApplication.java
+++ src/main/java/com/example/course_work/HelloApplication.java
@@ -15,7 +15,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader  =  new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 501, 442);
 
-        stage.setTitle("fgguffuuuhhu");
+        stage.setTitle("Messenger");
         stage.setScene(scene);
         stage.show();
     }
